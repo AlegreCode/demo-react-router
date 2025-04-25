911015f7dc4a2b5c3ad78a63f467dab4658d0bf8
Carlos Alberto Alegre <ca.alegre269@gmail.com>
2025-04-25 00:45:49 -0300

    feat: initial commit

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,15 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, layout, prefix, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+    //HOME
+    index("routes/home.tsx"),
+
+    //AUTH
+    ...prefix("auth", [
+        layout("layouts/auth-layout.tsx", [
+            route("login", "routes/auth/login-page.tsx"),
+            route("register", "routes/auth/register-page.tsx"),
+        ])
+    ])
+
+] satisfies RouteConfig;
