9b3d843aa5961f57f041bb91504a33a9664c42b7
Carlos Alberto Alegre <ca.alegre269@gmail.com>
2025-04-26 18:52:17 -0300

    fix: added chat folder

--- app/layouts/chat-layout.tsx
+++ app/layouts/chat-layout.tsx
@@ -1,7 +1,7 @@
-import { ScrollArea } from '~/components/ui/scroll-area'
 import { LogOut, X } from 'lucide-react'
-import { NavLink, Outlet, useParams } from 'react-router'
+import { Outlet, useParams } from 'react-router'
 import { Button } from '~/components/ui/button'
+import ContactList from '~/chat/components/ContactList'
 
 
 const ChatLayout = () => {
@@ -16,61 +16,7 @@ const ChatLayout = () => {
                         <span className="font-semibold">NexTalk</span>
                     </div>
                 </div>
-                <ScrollArea className="h-[calc(100vh-110px)]">
-                    <div className="space-y-4 p-4">
-                        <div className="space-y-1">
-                            <h3 className="px-2 text-sm font-semibold">Contacts</h3>
-                            <div className="space-y-1">
-                                <NavLink to={"/chat/ABC"} 
-                                    className={({ isActive }) => isActive ? "flex w-full justify-start bg-blue-500 p-2 rounded-2xl transition-colors duration-300" : "flex w-full justify-start p-2"}>
-                                    <div className="h-6 w-6 rounded-full bg-blue-500 mr-2 flex-shrink-0 flex items-center justify-center text-white text-xs">
-                                        G5
-                                    </div>
-                                    G5 Customer
-                                </NavLink>
-                                <NavLink to={"/chat"} className="flex w-full justify-start">
-                                    <div className="h-6 w-6 rounded-full bg-green-500 mr-2 flex-shrink-0 flex items-center justify-center text-white text-xs">
-                                        JD
-                                    </div>
-                                    John Doe
-                                </NavLink>
-                                <Button variant="ghost" className="w-full justify-start">
-                                    <div className="h-6 w-6 rounded-full bg-purple-500 mr-2 flex-shrink-0 flex items-center justify-center text-white text-xs">
-                                        AS
-                                    </div>
-                                    Alice Smith
-                                </Button>
-                                <Button variant="ghost" className="w-full justify-start">
-                                    <div className="h-6 w-6 rounded-full bg-yellow-500 mr-2 flex-shrink-0 flex items-center justify-center text-white text-xs">
-                                        RJ
-                                    </div>
-                                    Robert Johnson
-                                </Button>
-                                <Button variant="ghost" className="w-full justify-start">
-                                    <div className="h-6 w-6 rounded-full bg-pink-500 mr-2 flex-shrink-0 flex items-center justify-center text-white text-xs">
-                                        EW
-                                    </div>
-                                    Emma Wilson
-                                </Button>
-                            </div>
-                        </div>
-                        <div className="pt-4 border-t mt-4">
-                            <h3 className="px-2 text-sm font-semibold mb-1">Recent</h3>
-                            <Button variant="ghost" className="w-full justify-start">
-                                <div className="h-6 w-6 rounded-full bg-gray-500 mr-2 flex-shrink-0 flex items-center justify-center text-white text-xs">
-                                    TM
-                                </div>
-                                Thomas Miller
-                            </Button>
-                            <Button variant="ghost" className="w-full justify-start">
-                                <div className="h-6 w-6 rounded-full bg-red-500 mr-2 flex-shrink-0 flex items-center justify-center text-white text-xs">
-                                    SB
-                                </div>
-                                Sarah Brown
-                            </Button>
-                        </div>
-                    </div>
-                </ScrollArea>
+                <ContactList />
                 <div className='w-full p-1.5 border-t'>
                     <Button variant={"destructive"} size={"sm"} className='text-white w-full hover:cursor-pointer'>
                         <LogOut className="h-4 w-4 mr-2" /> Log out
