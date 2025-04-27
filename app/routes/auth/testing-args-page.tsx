94af483a6c3777883fba536b421132812b56f2c6
Carlos Alberto Alegre <ca.alegre269@gmail.com>
2025-04-27 19:29:26 -0300

    fix: more updates

--- app/routes/auth/testing-args-page.tsx
+++ app/routes/auth/testing-args-page.tsx
@@ -1,19 +1,45 @@
 import { Link } from "react-router";
 import type { Route } from "./+types/testing-args-page"
 import { Button } from "~/components/ui/button";
+import { Loader2 } from "lucide-react";
+import { sleep } from "~/lib/sleep";
 
 
-export async function loader() {
-  return { message: "Hola Mundo, desde el server.!" }
+export function meta() {
+  return [
+    { title: "Support chat" },
+    {
+      property: "og:title",
+      content: "Very cool app",
+    },
+    {
+      name: "description",
+      content: "This app is the best",
+    },
+  ];
 }
 
-export async function clientLoader({ serverLoader }: Route.ClientLoaderArgs) {
+// export async function loader() {
+//   return { message: "Hola Mundo, desde el server.!" }
+// }
+
+export async function clientLoader() {
+  await sleep(3000)
   // call the server loader
-  const serverData = await serverLoader();
+  // const serverData = await serverLoader();
   // And/or fetch data on the client
   // const data = getDataFromClient();
   // Return the data to expose through useLoaderData()
-  return { message: "Hola Mundo, desde el cliente.!", serverData }
+  return { message: "Hola Mundo, desde el cliente.!" }
+}
+
+export function HydrateFallback() {
+  return (
+    <div className="flex flex-col items-center justify-center gap-2 fixed inset-0 bg-white/80 backdrop-blur-sm">
+      <Loader2 className="h-16 w-16 animate-spin text-sky-500" />
+      <p className="text-2xl font-medium animate-pulse">Cargando...</p>
+    </div>
+  );
 }
 
 
