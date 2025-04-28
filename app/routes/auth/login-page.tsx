5527eb704e95357e1fcbd911ef4559be7c975484
Carlos Alberto Alegre <ca.alegre269@gmail.com>
2025-04-28 17:45:34 -0300

    fix: login page

--- app/routes/auth/login-page.tsx
+++ app/routes/auth/login-page.tsx
@@ -3,7 +3,19 @@ import { Card, CardContent } from "~/components/ui/card"
 import { Input } from "~/components/ui/input"
 import { Label } from "~/components/ui/label"
 import placeholderImg from "~/assets/images/placeholder.svg"
-import { Link, useNavigate } from "react-router"
+import { Link, redirect, useNavigate } from "react-router"
+import type { Route } from "./+types/login-page"
+import { getSession } from "~/sessions.server"
+
+export async function loader({ request }: Route.LoaderArgs){
+  const session = await getSession(request.headers.get("Cookie"))
+
+  console.log("session", session.get("userId"))
+
+  if (session.get("userId")) {
+    return redirect("/chat")
+  }
+}
 
 const LoginPage = () => {
   const navigate = useNavigate()
