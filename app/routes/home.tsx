cfe69de25ce0855d3b0ebe11d8415d0cda77e265
Carlos Alberto Alegre <ca.alegre269@gmail.com>
2025-04-28 17:25:04 -0300

    feat: redirect to chat page.

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,10 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
+import { redirect } from "react-router";
+
+export function loader() {
+  return redirect("/chat")
+}
 
 export function meta({}: Route.MetaArgs) {
   return [
