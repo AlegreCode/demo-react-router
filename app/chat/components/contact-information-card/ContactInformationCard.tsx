61c482c4f5141d460d862ba999b6d64b51eb3d77
Carlos Alberto Alegre <ca.alegre269@gmail.com>
2025-04-29 17:51:10 -0300

    fix: update contact information

--- app/chat/components/contact-information-card/ContactInformationCard.tsx
+++ app/chat/components/contact-information-card/ContactInformationCard.tsx
@@ -1,11 +1,23 @@
+import { useLoaderData, useNavigation, useParams } from 'react-router'
 import ContactInformation from './ContactInformation'
 import ContactInformationSkeleton from './ContactInformationSkeleton'
 import NoContactSelected from './NoContactSelected'
+import type { Client } from '~/chat/interfaces/chat.interface'
 
 const ContactInformationCard = () => {
-    return <NoContactSelected />
+    const { id } = useParams()
+    const { clients = [] } = useLoaderData()
+    const { state } = useNavigation()
+    const isPending = state === "loading"
+
+    if (isPending) return <ContactInformationSkeleton />
+
+    if (!id) return <NoContactSelected />
+
+    const client = clients.find((client: Client) => client.id === id)
+
     return (
-        <ContactInformation />
+        <ContactInformation client={client}/>
     )
 }
 
