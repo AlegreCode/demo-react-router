44fb36e77862a4ac4049e9b4bdcb8f0cc2a2627e
Carlos Alberto Alegre <ca.alegre269@gmail.com>
2025-05-01 18:42:25 -0300

    fix: update session

--- app/sessions.server.ts
+++ app/sessions.server.ts
@@ -19,6 +19,7 @@ const { getSession, commitSession, destroySession } =
 
         // all of these are optional
         domain: "demo-react-router.onrender.com",
+        // domain: "localhost",
         // Expires can also be set (although maxAge overrides it when used in combination).
         // Note that this method is NOT recommended as `new Date` creates only one date on each server deployment, not a dynamic date in the future!
         //
